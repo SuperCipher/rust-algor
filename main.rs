fe8d4db03b9bce66b299686c511960fa0f84b733
nach <napat_pat3@hotmail.com>
2022-06-14 20:29:43 +0700

    feat: find two sum in one function

--- main.rs
+++ main.rs
@@ -2,23 +2,20 @@ fn main() {
     let input: Vec<i32> = vec![2, 7, 11, 15];
     let target: i32 = 9;
 
-    // let pair_result: Vec<(i32, i32)> = pair_combi(input.len() as i32);
-    // println!("pair_combi {:?}", pair_result);
     println!("input {:?}", input);
     let two_sum_list2 = two_sum(input, target);
     println!("two_sum_list2 {:?}", two_sum_list2);
 
     let maybe_index: Option<(i32, i32)> = two_sum_list2;
-    // let index: usize = match maybe_index {
-    //     // The division was valid
-    //     Some(x) => x,
-    //     // The division was invalid
-    //     None => 0,
-    // };
-    // let (l_result, r_result) = pair_result[index];
-    // let result: Vec<i32> = [l_result, r_result].to_vec();
-    // assert!(result == [0, 1]);
-    // println!("result {:?}", result);
+    let index_pair: (i32, i32) = match maybe_index {
+        // The division was valid
+        Some(tuple) => tuple,
+        // The division was invalid
+        None => (0, 0),
+    };
+    let (l_result, r_result) = index_pair;
+    let result: Vec<i32> = [l_result, r_result].to_vec();
+    assert!(result == vec![0, 1]);
 }
 
 fn two_sum(input: Vec<i32>, target: i32) -> Option<(i32, i32)> {
@@ -27,12 +24,5 @@ fn two_sum(input: Vec<i32>, target: i32) -> Option<(i32, i32)> {
     let maybe_index = (0..(n as i32))
         .flat_map(|x| (0..=x - 1).map(move |y| (y, x))) // pair of possible combination
         .find(|(x, y)| (input[*x as usize] + input[*y as usize]) == target);
-    // .find_map(|t| t == target);
-    // let index: usize = match maybe_index {
-    //     // The division was valid
-    //     Some(x) => x,
-    //     // The division was invalid
-    //     None => 0,
-    // };
     return maybe_index;
 }
